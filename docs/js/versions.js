51c73ef726fdadc6de16f8aa5e0ba4137702cb7b
ndw <ndw@nwalsh.com>
2024-11-09 13:57:31 +0000

    Deploying to gh-pages from @ xmlcalabash/docs@1c1c5a6fffae28b464ff1d8062b95d43aaa7a6f6 🚀

--- docs/js/versions.js
+++ docs/js/versions.js
@@ -1,6 +1,7 @@
 (function() {
   const gver = document.querySelector("#guideversion");
   const rver = document.querySelector("#refversion");
+  const aver = document.querySelector("#apiversion");
 
   fetch("https://docs.xmlcalabash.com/userguide/current/details.json")
     .then(response => {
@@ -16,6 +17,15 @@
       if (response.ok) {
         response.json().then(json => {
           rver.innerHTML = `<a href="/reference/${json.version}/">Version ${json.version}</a>`
+        })
+      }
+    });
+
+  fetch("https://docs.xmlcalabash.com/apidocs/current/details.json")
+    .then(response => {
+      if (response.ok) {
+        response.json().then(json => {
+          aver.innerHTML = `<a href="/apidocs/${json.version}/">Version ${json.version}</a>`
         })
       }
     });
